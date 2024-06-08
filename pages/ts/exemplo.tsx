a0adc9b6228952d18e336c78290380a7d173e7a5
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-08 15:15:28 -0300

    Adicionando exemplos com typescript

---
+++ pages/ts/exemplo.tsx
@@ -0,0 +1,10 @@
+import Pessoa from "../../components/ts/Pessoa";
+
+export default function exemploTS() {
+    return (
+        <div>
+            <Pessoa nome={"Fellipe"} idade={31}/>
+            <Pessoa nome={"João"}/>
+        </div>
+    )
+}
